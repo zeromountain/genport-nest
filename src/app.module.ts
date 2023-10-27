e6c3e6a34711b698b5850a48f4aaac8beaa98542
zeromountain <sonyeongsan@gmail.com>
2023-10-27 16:36:50 +0900

    modify app function

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,51 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersService } from './users/users.service';
-import { UsersController } from './users/users.controller';
-import { UsersModule } from './users/users.module';
+// import { UsersService } from './users/users.service';
+// import { UsersController } from './users/users.controller';
 import { LoggerMiddleware } from './logger/logger.middleware';
-import { AuthController } from './auth/auth.controller';
-import { AuthService } from './auth/auth.service';
+// import { AuthController } from './auth/auth.controller';
+// import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule } from '@nestjs/config';
+import { UserModel } from './users/entities/user.entity';
+import { UsersModule } from './users/users.module';
+import { JwtAccessStrategy } from './auth/utils/jwt-access.strategy';
+import { JwtRefreshStrategy } from './auth/utils/jwt-refresh.strategy';
 
 @Module({
-  imports: [UsersModule, AuthModule],
-  controllers: [AppController, UsersController, AuthController],
-  providers: [AppService, UsersService, AuthService],
+  imports: [
+    AuthModule,
+    UsersModule,
+    ConfigModule.forRoot(
+      process.env.NODE_ENV === 'production'
+        ? {
+            envFilePath: '.production.env',
+          }
+        : {
+            envFilePath: '.development.env',
+          },
+    ),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [UserModel],
+      synchronize: true,
+    }),
+    PassportModule.register({ session: true }),
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    JwtAccessStrategy, //
+    JwtRefreshStrategy,
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
