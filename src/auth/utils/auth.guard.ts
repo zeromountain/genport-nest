550216c201ef2d692736e35d873b324cd4c77e7f
zeromountain <sonyeongsan@gmail.com>
2023-10-27 16:34:48 +0900

    add google auth guard

---
+++ src/auth/utils/auth.guard.ts
@@ -0,0 +1,16 @@
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+// ... 생략 ...
+@Injectable()
+// ❶ google 스트래티지 사용
+export class GoogleAuthGuard extends AuthGuard('google') {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    // ❷ 부모 클래스의 메서드 사용
+    const result = (await super.canActivate(context)) as boolean;
+    // ❸ 컨텍스트에서 리퀘스트 객체를 꺼냄
+    const request = context.switchToHttp().getRequest();
+    await super.logIn(request);
+    return result;
+  }
+}
